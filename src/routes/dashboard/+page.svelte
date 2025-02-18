c7f3b0b9e26b92421a2e42237259c7306cd5fa6f
roni <rtlockwoodwork@gmail.com>
2025-02-17 18:09:55 -0700

    remove tailwind pkg, add grid to dashboard

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -113,8 +113,10 @@
     }
 </script>
 
-<div class="max-w-7xl mx-auto p-6">
-  <div class="mb-12">
+<div class="container mx-auto p-6">
+  <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mb-12">
+    
+  <div class="mb-12 w-2/3">
     <h1 class="text-2xl font-bold mb-6">Add New Project</h1>
     <ProjectForm />
   </div>
@@ -141,4 +143,5 @@
       </div>
     {/if}
   </div>
+</div>
 </div>
